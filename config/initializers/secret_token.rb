ceda76c672f26d3d4b899fd3d6ec39f3830cf4d1
Kyle Braatz <kyle@healthwave.co>
2013-06-15 13:55:29 -0400

    Created a Rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hwsocial::Application.config.secret_token = '94b18a6c60eca05345dca9ff825734a9513b2e460da6f4d622729956102a8e98c68aced4f79f4a0087a514b52459c30397a3e6e6096045a646d40812b6d9a038'
